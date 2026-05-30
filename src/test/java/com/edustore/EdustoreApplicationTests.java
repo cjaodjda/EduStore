2950100b1484d0f186caac4e79396472fb5cc68b
cjaodjdo <wujiukaiben@163.com>
2026-05-30 11:22:44 +0800

    chore: 初始化 Spring Boot 项目，添加 web/mysql/lombok/validation 依赖

---
+++ src/test/java/com/edustore/EdustoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edustore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdustoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
